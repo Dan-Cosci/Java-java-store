b21b697047f6fa96ba771c85969a249c1d38588e
Daniel <allinasdaniel@gmail.com>
2025-04-24 18:04:38 +0800

    added the object classes used for database items

---
+++ src/Objects/InvItem.java
@@ -0,0 +1,15 @@
+package Objects;
+
+public class InvItem {
+
+    private int id;
+    private String item;
+    private float price;
+    private int quantity;
+    
+    public InvItem() {
+        // TODO
+    }
+    
+    
+}
---
+++ src/Objects/TransReciept.java
@@ -0,0 +1,15 @@
+package Objects;
+
+public class TransReciept {
+    
+    private int id;
+    private int item_id;
+    private int user_id;
+    private int quantity;
+    //  date will be using the built in sqlite command
+    
+    public TransReciept() {
+        // TODO
+    }
+    
+}
