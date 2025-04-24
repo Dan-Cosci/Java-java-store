e8f07331baa515db730f078c29fd61e2e938b25e
Daniel <allinasdaniel@gmail.com>
2025-04-24 11:57:03 +0800

    created the framework for the database

--- src/Database/DBHandler.java
+++ src/Database/DBHandler.java
@@ -1,5 +1,30 @@
 package Database;
 
+import java.sql.*;
+
 public class DBHandler {
+    
+    Connection con = null;
+    private final String url = "jdbc:sqlite:src/Database/database.db";
+    
+    public DBHandler() {
+        try {
+            getconnection();
+        } catch (SQLException ex) {
+            System.err.println("Error to connect to database: " + ex);
+        }
+    }
+    
+    public void getconnection() throws SQLException{
+        con = DriverManager.getConnection(url);
+    }
+    
+    public void close(){
+        try {
+            con.close();
+        } catch (SQLException ex) {
+            System.err.println("Failed to close database: " + ex);
+        }
+    }
     
 }
